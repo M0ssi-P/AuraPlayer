e97159e55c4815c4bca7aa09502380ef73a3fbac
M0ssi-P <jarthensmith2004@gmail.com>
2026-03-18 22:46:37 +1100

    moving to ma

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,11 +1,11 @@
 pluginManagement {
     repositories {
+        mavenCentral()
         maven("https://jitpack.io")
         maven("https://packages.jetbrains.team/maven/p/kpm/public")
         maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
         google()
         gradlePluginPortal()
-        mavenCentral()
     }
 
     plugins {
